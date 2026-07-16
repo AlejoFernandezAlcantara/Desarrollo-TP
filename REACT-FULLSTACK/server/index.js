84d877642ac788d40edbfb0f30eadc9628cd6049
Alejofernandezalcantara <alejofernandezalcantara@gmail.com>
2026-07-16 11:50:45 -0300

    Agregar .gitignore, sacar node_modules del tracking y configurar Sequelize con dotenv

--- REACT-FULLSTACK/server/index.js
+++ REACT-FULLSTACK/server/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 
+const bd = require('./models')
+
+
+
+bd.sequelize.sync().then(() => {
 app.listen(3001, () => {
-    
   console.log('Servidor corriendo en el puerto 3001');
 
+  });
 });
